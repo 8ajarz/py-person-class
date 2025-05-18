22ab95c60f233f7b89b290954fa3672a95ab4f3e
8ajarz <8ajarz_zaprasza@protonmail.com>
2025-05-18 13:10:14 +0700

    adjasted the create_person_list logic to align with AI mentor requirements

--- app/main.py
+++ app/main.py
@@ -8,12 +8,13 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    marriage = ("wife", "husband")
     friends = [Person(person["name"], person["age"]) for person in people]
     for friend in friends:
         person = [dct for dct in people if dct["name"] == friend.name][0]
-        spouse = marriage["husband" in person]
-        partner = spouse, person[spouse]
+        if "husband" in person:
+            partner = "husband", person["husband"]
+        else:
+            partner = "wife", person["wife"]
         if partner[1] is not None:
             setattr(friend, partner[0], Person.people[partner[1]])
     return friends
